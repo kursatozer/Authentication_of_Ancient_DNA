642b315712fa7c637859cd0aab9735f3d64aeb43
kursatozer <90481141+kursatozer@users.noreply.github.com>
2022-04-15 16:16:06 +0300

    Update authentication.R

--- authentication.R
+++ authentication.R
@@ -1,15 +1,17 @@
-
 library(Rsamtools)
 library(magrittr)
 library(ggplot2)
 library(ggpubr)
 
-TAXID=
-FOLDER=""
+args <- commandArgs(trailingOnly = TRUE)
 
+TAXID= args[1] #1302
+FOLDER<-args[2] #="itk095-b1e1l1p1_TAATGCG-TTACTTA_L002_ARmerged.210803_A00187_0543_BHCFVTDRXY.all.fastq.gz_PipelineOutput"
+OUTFILE <- paste0(TAXID,"_",FOLDER, ".pdf")
 
+cat(TAXID, FOLDER, OUTFILE, "\n")
 BAM=paste0(FOLDER,"/",TAXID,".output.sorted.bam")
-MAPDAMAGE=paste0(FOLDER,"/","results_./", TAXID, ".output.bam")
+mapdamage=paste0(FOLDER,"/","results_./", TAXID, ".output.bam")
 
 #BAM="itk095-b1e1l1p1_TAATGCG-TTACTTA_L002_ARmerged.210803_A00187_0543_BHCFVTDRXY.all.fastq.gz_PipelineOutput/1302.output.sorted.bam"
 
@@ -40,11 +42,13 @@ length_plot <- ggplot(data = length, aes(x=Length, y = Frequency)) +
   geom_bar(stat="identity") 
 
 
-deamination5p <- read.table(paste0(mapdamage, "5pCtoT_freq.txt"), header=T, col.names = c("Position", "C_T"))
-deamination3p <- read.table(paste0(mapdamage, "3pGtoA_freq.txt"), header=T, col.names = c("Position", "G_A"))
+deamination5p <- read.table(paste0(mapdamage, "/5pCtoT_freq.txt"), header=T, col.names = c("Position", "C_T"))
+deamination3p <- read.table(paste0(mapdamage, "/3pGtoA_freq.txt"), header=T, col.names = c("Position", "G_A"))
 
 plot_5p <- ggplot(deamination5p,aes(x=Position, y=C_T)) + geom_line(color="red") +  xlab("Distance from 5' end") + ylab("C>T transition rate")
 plot_3p <- ggplot(deamination3p,aes(x=Position, y=G_A)) + geom_line(color="blue") +  xlab("Distance from 3' end") + ylab("G>A transition rate") + scale_x_reverse() + 
   scale_y_continuous(position = "right")
 
 last_plot <- ggarrange(edit_plot, length_plot, plot_5p, plot_3p, labels = c("a","b","c"))
+
+ggsave(filename = OUTFILE, plot = last_plot, device = "pdf", width = 4*3, height = 3*3)
